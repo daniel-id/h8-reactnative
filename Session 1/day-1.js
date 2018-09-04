5de247e8ff6f6d99acfbb84fa18be32f0d9ce97a
daniel-id <daniels@outlook.co.id>
2018-09-04 07:29:20 +0700

    Initial commit

--- Session 1/day-1.js
+++ Session 1/day-1.js
@@ -1,44 +1,44 @@
-// ===== Prompt =====
+===== Prompt =====
 
-// let prompt = require('prompt');
-//
-// prompt.start();
-//
-// prompt.get(['username', 'email'], function (err, result) {
-//   console.log(result.username);
-//   console.log(result.email);
-// });
+let prompt = require('prompt');
 
-// ===== Dynamic Object =====
+prompt.start();
 
-// let obj = {
-//   [key]: value,
-// };
-//
-// let obj = {};
-// obj[key] = value;
+prompt.get(['username', 'email'], function (err, result) {
+  console.log(result.username);
+  console.log(result.email);
+});
 
-// let fillAnObject = (key, value) => {
-//   let obj = {};
-//   obj[key] = value;
-//
-//   return obj;
-// };
-//
-// console.log(fillAnObject('nama', 'Fadly'));
+===== Dynamic Object =====
 
-// let fillAnObject = (arrayKey, arrayValue) => {
-//   let obj = {};
-//
-//   for (i = 0; i < arrayKey.length; i++) {
-//     obj[arrayKey[i]] = arrayValue[i];
-//   };
-//
-//   return obj;
-// };
-//
-// console.log(fillAnObject(['nama', 'alamat'], ['Fadly', 'Pondok Indah']));
+let obj = {
+  [key]: value,
+};
 
-// ===== Array index with string =====
+let obj = {};
+obj[key] = value;
 
-// keyword: get array index with string javascript
+let fillAnObject = (key, value) => {
+  let obj = {};
+  obj[key] = value;
+
+  return obj;
+};
+
+console.log(fillAnObject('nama', 'Fadly'));
+
+let fillAnObject = (arrayKey, arrayValue) => {
+  let obj = {};
+
+  for (i = 0; i < arrayKey.length; i++) {
+    obj[arrayKey[i]] = arrayValue[i];
+  };
+
+  return obj;
+};
+
+console.log(fillAnObject(['nama', 'alamat'], ['Fadly', 'Pondok Indah']));
+
+===== Array index with string =====
+
+keyword: get array index with string javascript
